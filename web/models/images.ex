84d9a5db1efb6cc1676aa37eaca835a961061f0f
Michael Ries <michael@riesd.com>
2016-07-19 23:15:21 -0600

    randomly select and error image when we fail to create a nomination

---
+++ web/models/images.ex
@@ -0,0 +1,16 @@
+defmodule Oprah.Images do
+  import Oprah.Router.Helpers
+
+  def error_url(conn) do
+    random_image_path(conn, "errors")
+  end
+
+  defp random_image_path(conn, group) do
+    :rand.seed(:exsplus, :erlang.now)
+    relative_dir = "priv/static/images/#{group}"
+    filepath = Path.join([__DIR__, "../../", relative_dir])
+    basenames = File.ls!(filepath) |> Enum.filter(&( !String.starts_with?(&1, ".") ))
+    basename = Enum.shuffle(basenames) |> List.first
+    static_path(conn, "/images/#{group}/#{basename}")
+  end
+end
--- web/models/nomination.ex
+++ web/models/nomination.ex
@@ -17,5 +17,14 @@ defmodule Oprah.Nomination do
     struct
     |> cast(params, [:body, :awarded_at, :nominee_id, :nominated_by_id])
     |> validate_required([:body, :nominee_id, :nominated_by_id])
+    |> do_not_nominate_yourself
+  end
+
+  defp do_not_nominate_yourself(changeset) do
+    if fetch_field(changeset, :nominee_id) == fetch_field(changeset, :nominated_by_id) do
+      add_error(changeset, :nominee_id, "You can't nominate yourself")
+    else
+      changeset
+    end
   end
 end
