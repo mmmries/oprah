b2e72757d6d9781c1b98c5c030906159a5a0237d
Michael Ries <michael@riesd.com>
2016-07-22 23:22:23 -0600

    remove user create/registration stuff

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,28 +1,10 @@
 defmodule Oprah.UserController do
   use Oprah.Web, :controller
-  plug :require_current_user when action != :create
+  plug :require_current_user
 
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
-  end
-
-  def new(conn, _params) do
-    changeset = User.changeset(%User{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
-
-    case Repo.insert(changeset) do
-      {:ok, _user} ->
-        conn
-        |> put_flash(:info, "User created successfully.")
-        |> redirect(to: user_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
   end
 
   def show(conn, %{"id" => id}) do
