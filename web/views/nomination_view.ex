8059be4828a111af8bd7fcdfe0842c3e8b86c687
Michael Ries <michael@riesd.com>
2016-08-11 22:37:27 -0600

    sort Ecto.DateTime by first casting them to erlang date time tuples (which are sortable)

--- web/views/nomination_view.ex
+++ web/views/nomination_view.ex
@@ -2,6 +2,6 @@ defmodule Oprah.NominationView do
   use Oprah.Web, :view
 
   def reversed_keys(map) do
-    Map.keys(map) |> Enum.sort |> Enum.reverse
+    Map.keys(map) |> Enum.sort_by(&( Ecto.DateTime.to_erl(&1) )) |> Enum.reverse
   end
 end
