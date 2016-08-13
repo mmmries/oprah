05978214a4fcf8ee06f2922dd0fca7cefe4d3a89
Michael Ries <michael@riesd.com>
2016-08-12 19:31:09 -0600

    add credo to the project

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Oprah.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:earmark, "~> 1.0"},
+    [{:credo, "~> 0.4.8"},
+     {:earmark, "~> 1.0"},
      {:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
