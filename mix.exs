71066c18bc76cea4803e0fed3a3f0fd92fa202b0
Michael Ries <michael@riesd.com>
2017-02-03 10:50:02 -0700

    lock down ueberauth related stuff, new version breaks for github enterprise

--- mix.exs
+++ mix.exs
@@ -30,17 +30,22 @@ defmodule Oprah.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:credo, "~> 0.4.8"},
-     {:earmark, "~> 1.0"},
-     {:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:ueberauth_github, "~> 0.2"}]
+    [
+      {:credo, "~> 0.4.8"},
+      {:earmark, "~> 1.0"},
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      # locking this down, until we I can figure out how to upgrade safely
+      {:ueberauth_github, "0.2.0"},
+      {:ueberauth, "0.3.0"},
+      {:oauth2, "0.6.0"},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
