84127938dd13f762c1a1a2306b838c2e6160e1a4
Jon Carstens <jon.carstens@mx.com>
2017-03-24 15:37:53 -0600

    fix tests

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -6,16 +6,16 @@ defmodule Oprah.ErrorViewTest do
 
   test "renders 404.html" do
     assert render_to_string(Oprah.ErrorView, "404.html", []) ==
-           "Page not found"
+           "<html><body><h1>Page not found</h1><br/><img src='/images/i_will_not_accept_that.gif' /></body></html>"
   end
 
   test "render 500.html" do
     assert render_to_string(Oprah.ErrorView, "500.html", []) ==
-           "Internal server error"
+           "<html><body><h1>Internal server error</h1><br/><img src='/images/i_will_not_accept_that.gif' /></body></html>"
   end
 
   test "render any other" do
     assert render_to_string(Oprah.ErrorView, "505.html", []) ==
-           "Internal server error"
+           "<html><body><h1>Internal server error</h1><br/><img src='/images/i_will_not_accept_that.gif' /></body></html>"
   end
 end
