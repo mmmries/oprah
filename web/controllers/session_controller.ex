b0888bf56bcfc97270ce1e6569ab732448cf8dcd
Jon Carstens <jon.carstens@mx.com>
2017-03-30 14:50:07 -0600

    find user by gitlab_id

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -13,8 +13,8 @@ defmodule Oprah.SessionController do
   def callback(%{assigns: %{ueberauth_failure: %{errors: errors}}}=conn, _params) do
     render(conn, :error, errors: errors)
   end
-  def callback(%{assigns: %{ueberauth_auth: %{info: %{name: name}, uid: uid}}}=conn, _params) do
-    user = Oprah.User.user_from_uberauth_info(uid, name)
+  def callback(%{assigns: %{ueberauth_auth: %{info: %{name: name}, uid: gitlab_id}}}=conn, _params) do
+    user = Oprah.User.user_from_uberauth_info(gitlab_id, name)
     conn |> put_session(:user_id, user.id) |> put_flash(:info, "Yo #{user.name}") |> redirect(to: nomination_path(conn, :pick_a_nominee))
   end
 end
